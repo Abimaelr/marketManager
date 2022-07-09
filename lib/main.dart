c480bd8201dae5ae9001af470b4967f5782fdc65
Abimael Albuquerque <abimael.albuquerque@colab.re>
2022-07-09 12:16:52 -0400

    Add project

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // ignore: use_key_in_widget_constructors
+  const MyApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          
+        )
+      ),
+    );
+  }
+}
